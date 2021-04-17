4505a5a9a6bb2284ce6f08611066651b6f165bbe
Cem YETEN <cem@verimsoft.com>
2021-04-17 19:19:09 +0300

    Add project files.

---
+++ API.data/DataContext/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿
+using API.data.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.data.DataContext
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }    
+
+    
+}
