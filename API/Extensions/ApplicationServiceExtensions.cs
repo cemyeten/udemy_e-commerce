a5cdcca1f54bd6038bf1ad75921dc7d5e2b95e38
Cem YETEN <cem@verimsoft.com>
2021-04-20 20:07:15 +0300

    swagger dokumantasyonu eklendi middleware'ler yazıldı

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using API.Core.Interfaces;
+using API.Infastructure.Implements;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplciationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            return services;
+
+        }
+    }
+}
